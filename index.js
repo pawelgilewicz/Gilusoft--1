9303b92d7526f6ef37d36ab31789bb06c04d5f56
Pawel <pawelgilewicz2009@gmail.com>
2026-07-26 22:37:58 +0200

    oauth google passport

--- index.js
+++ index.js
@@ -206,7 +206,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/gilusoft",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL ,//"http://localhost:3000/auth/google/gilusoft",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
