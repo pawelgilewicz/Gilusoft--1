a77304e2d3df0759e2b962146f5db8005381e841
Pawel <pawelgilewicz2009@gmail.com>
2026-07-31 15:40:30 +0200

    ssl service added

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ const db = new pg.Client({
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
-  // ssl: {
-  //   rejectUnauthorized: false
-  // }
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 db.connect();
 
